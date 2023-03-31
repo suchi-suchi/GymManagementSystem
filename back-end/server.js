db870915bed1aed21755d6b193cc4b04f9808002
Madhuri Ghattamaneni <madhurig9999@gmail.com>
2023-03-30 17:07:54 -0700

    Add Admin Api

--- back-end/server.js
+++ back-end/server.js
@@ -314,6 +314,19 @@ mongoose.connect("mongodb+srv://suchandranathbajjuri:Suchi7@cluster202.v83m9mk.m
     res.status(500).json({message: error.message})
   }
 })
+
+ // ------------------------------- Admin specific endpoints -----------------------------------
+
+    // add admin 
+    app.post('/addAdmin',async(req,res)=>{
+      try {
+        const admin = await Admin.create(req.body)
+        res.status(200).json(admin)
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error.message})
+      }
+    })
     
   }
   ).catch((error) => console.log("db connection error" + error));
