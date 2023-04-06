cbe94c38367ecc32df07f7f709ea3e6f01d76e49
Madhuri Ghattamaneni <madhurig9999@gmail.com>
2023-04-05 17:08:01 -0700

    add activity api

--- back-end/server.js
+++ back-end/server.js
@@ -400,5 +400,18 @@ mongoose.connect("mongodb+srv://suchandranathbajjuri:Suchi7@cluster202.v83m9mk.m
       }
     })
     
+  // ------------------------------- Activity specific endpoints -----------------------------------
+
+    // add activity 
+    app.post('/addActivity',async(req,res)=>{
+      try {
+        const activity = await Activity.create(req.body)
+        res.status(200).json(activity)
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error.message})
+      }
+    })
+
   }
   ).catch((error) => console.log("db connection error" + error));
