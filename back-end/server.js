d1bf7b5481fac5e9148e8009fc3ab492478d8bc9
yogeshluckshetty <yogesh.luckshetty@gmail.com>
2023-05-03 11:46:12 -0700

    log hours

--- back-end/server.js
+++ back-end/server.js
@@ -802,5 +802,29 @@ mongoose.connect("mongodb+srv://suchandranathbajjuri:Suchi7@cluster202.v83m9mk.m
       }
     })
 
+    // ------------------------------- logMachineTracking specific endpoints -----------------------------------
+
+    // add logMachineTracking 
+    app.post('/addlogMachineTracking', async (req, res) => {
+      try {
+        const logMachineTracking = await LogMachineTracking.create(req.body)
+        res.status(200).json(logMachineTracking)
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+      }
+    })
+
+    // gets all logMachineTracking information
+    app.get('/logMachineTracking', async (req, res) => {
+      try {
+        const logMachineTrackings = await LogMachineTracking.find({})
+        res.status(200).json(logMachineTrackings)
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+      }
+    })
+
   }
   ).catch((error) => console.log("db connection error" + error));
